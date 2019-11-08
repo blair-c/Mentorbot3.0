6b007fdb3deb6cb6c4b4711c0647baee26a59cf9
Blair C <bscardiel@gmail.com>
2019-11-07 19:27:48 -0800

    Try out walrus operator

--- cogs/actionlog.py
+++ cogs/actionlog.py
@@ -16,10 +16,8 @@ class ActionLog(commands.Cog):
     async def on_member_join(self, member):
         """Log that member has joined."""
         # Check that action-log channel exists and is viewable
-        guild = member.guild
-        if not guild: return
-        action_log = discord.utils.get(guild.text_channels, name='action-log')
-        if not action_log: return
+        if not guild:= member.guild: return
+        if not action_log := discord.utils.get(guild.text_channels, name='action-log'): return
         # Log member join
         embed = discord.Embed(
             color=0x66bb6a,
@@ -34,10 +32,8 @@ class ActionLog(commands.Cog):
     async def on_member_remove(self, member):
         """Display in action-log channel that member has left."""
         # Check that action-log channel exists and is viewable
-        guild = member.guild
-        if not guild: return
-        action_log = discord.utils.get(guild.text_channels, name='action-log')
-        if not action_log: return
+        if not guild:= member.guild: return
+        if not action_log := discord.utils.get(guild.text_channels, name='action-log'): return
         # Log member removal
         embed = discord.Embed(
             color=0xff7043,
@@ -62,10 +58,8 @@ class ActionLog(commands.Cog):
     async def on_message_delete(self, message):
         """Display info of deleted message in action-log channel."""
         # Check that action-log channel exists and is viewable
-        guild = message.guild
-        if not guild: return
-        action_log = discord.utils.get(guild.text_channels, name='action-log')
-        if not action_log: return
+        if not guild:= member.guild: return
+        if not action_log := discord.utils.get(guild.text_channels, name='action-log'): return
         # Ignore messages from boardroom
         if message.channel.name == 'boardroom': return
         # Log message deletion
@@ -86,10 +80,8 @@ class ActionLog(commands.Cog):
     async def on_message_edit(self, before, after):
         """Display info of edited message in action-log channel."""
         # Check that action-log channel exists and is viewable
-        guild = before.guild
-        if not guild: return
-        action_log = discord.utils.get(guild.text_channels, name='action-log')
-        if not action_log: return
+        if not guild:= member.guild: return
+        if not action_log := discord.utils.get(guild.text_channels, name='action-log'): return
         # Ignore messages from boardroom, and ensure that message content has changed
         if before.channel.name == 'boardroom' or before.content == after.content: return
         # Log message edit
@@ -113,10 +105,8 @@ class ActionLog(commands.Cog):
     async def on_member_update(self, before, after):
         """Display in action-log channel that member's nickname has changed."""
         # Check that action-log channel exists and is viewable
-        guild = before.guild
-        if not guild: return
-        action_log = discord.utils.get(guild.text_channels, name='action-log')
-        if not action_log: return
+        if not guild:= member.guild: return
+        if not action_log := discord.utils.get(guild.text_channels, name='action-log'): return
         # Ensure that nickname has changed
         if before.nick == after.nick: return
         # Log nickname change
