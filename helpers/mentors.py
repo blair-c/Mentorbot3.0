fb1723e9c934bf11025ed45255b8795fc02d9bb8
Blair C <bscardiel@gmail.com>
2019-11-16 00:15:16 -0800

    Bold mentor tags

--- helpers/mentors.py
+++ helpers/mentors.py
@@ -43,7 +43,7 @@ def mentors_of_status(ctx, cursor, status, c=None, r=None):
             try:
                 mentor = ctx.bot.get_user(row['discord_id'])
                 mentors.append(
-                    f"{mentor.mention} {str(mentor)} ({row['region']})")
+                    f"{mentor.mention} **{str(mentor)}** ({row['region']})")
             except AttributeError:  # catch if user ID isn't found, eg. left the server
                 mentors.append(f"{row['name']} ({row['region']})")
     elif r:  # If region was given
@@ -54,7 +54,7 @@ def mentors_of_status(ctx, cursor, status, c=None, r=None):
             try:
                 mentor = ctx.bot.get_user(row['discord_id'])
                 mentors.append(
-                    f"{mentor.mention} {str(mentor)} ({row['characters']})")
+                    f"{mentor.mention} **{str(mentor)}** ({row['characters']})")
             except AttributeError:  # catch if user ID isn't found, eg. left the server
                 mentors.append(f"{row['name']} ({row['characters']})")
     return '\n'.join(mentors)
