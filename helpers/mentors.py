f1d36b83f045cb700d26d266b7a9d7723f98816c
Blair C <bscardiel@gmail.com>
2020-10-16 07:26:50 -0700

    postgresql boolean

--- helpers/mentors.py
+++ helpers/mentors.py
@@ -41,7 +41,7 @@ def mentors_of_status(bot, status, character=None, region=None):
     mentors = []
     if character:
         sql = """SELECT discord_id, name, region, switch, xbox FROM mentors WHERE status
-                 = %(status)s AND characters LIKE %(character)s ESCAPE '' AND do_not_disturb = 0"""
+                 = %(status)s AND characters LIKE %(character)s ESCAPE '' AND NOT do_not_disturb"""
         data = {'status': status, 'character': f'%{character}%'}
         db.execute(sql, data)
         for row in db.fetchall():
