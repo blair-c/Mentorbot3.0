672d234d1bdc87d1618744f571fed6e8481844e1
Blair C <bscardiel@gmail.com>
2021-03-06 02:12:03 -0800

    revert discord.py

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ except ImportError:
     sys.exit(1)
 
 # Ensure correct Discord.py version
-if discord.__version__ != '1.6.0':
+if discord.__version__ != '1.5.1':
     print(f'Discord.py version: {discord.__version__}.\n'
-          'Please install version 1.6.0', file=sys.stderr)
+          'Please install version 1.5.1', file=sys.stderr)
     sys.exit(1)
 
 intents = discord.Intents.default()
