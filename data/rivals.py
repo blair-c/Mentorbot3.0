aa98bff9f80e618415183a363d073290c416908b
Blair C <bscardiel@gmail.com>
2022-01-31 19:12:26 -0800

    add new icons + update old to dedicated site

--- data/rivals.py
+++ data/rivals.py
@@ -3,92 +3,92 @@ characters = {
     'Zetterburn': {
         'color': 16742462,
         'emote': '<:Zetterburn:547189111052566556>',
-        'icon': 'https://i.imgur.com/aqLqIbB.png'
+        'icon': 'https://blair-c.github.io/roaa/img/zetterburn/icon.png'
     },
     'Forsburn': {
         'color': 11179322,
         'emote': '<:Forsburn:547189098859986984>',
-        'icon': 'https://i.imgur.com/wLU8CEL.png'
+        'icon': 'https://blair-c.github.io/roaa/img/forsburn/icon.png'
     },
     'Clairen': {
         'color': 16715114,
         'emote': '<:Clairen:547182529837400081>',
-        'icon': 'https://imgur.com/hfcf0vb.png'
+        'icon': 'https://blair-c.github.io/roaa/img/clairen/icon.png'
     },
     'Mollo': {
         'color': 16729927,
         'emote': '<:Mollo:937869128977510472>',
-        'icon': ''
+        'icon': 'https://blair-c.github.io/roaa/img/mollo/icon.png'
     },
     'Orcane': {
         'color': 4812504,
         'emote': '<:Orcane:547182506584047618>',
-        'icon': 'https://imgur.com/uF2DuY7.png'
+        'icon': 'https://blair-c.github.io/roaa/img/orcane/icon.png'
     },
     'Etalus': {
         'color': 6728666,
         'emote': '<:Etalus:547182495964069898>',
-        'icon': 'https://imgur.com/89ry8PT.png'
+        'icon': 'https://blair-c.github.io/roaa/img/etalus/icon.png'
     },
     'Ranno': {
         'color': 379087,
         'emote': '<:Ranno:547182486971613185>',
-        'icon': 'https://imgur.com/XNggZrr.png'
+        'icon': 'https://blair-c.github.io/roaa/img/ranno/icon.png'
     },
     'Hodan': {
         'color': 14024191,
         'emote': '<:Hodan:937869074778693632>',
-        'icon': ''
+        'icon': 'https://blair-c.github.io/roaa/img/hodan/icon.png'
     },
     'Kragg': {
         'color': 5866833,
         'emote': '<:Kragg:547182475835736155>',
-        'icon': 'https://imgur.com/qsnG9Xy.png'
+        'icon': 'https://blair-c.github.io/roaa/img/kragg/icon.png'
     },
     'Maypul': {
         'color': 8439641,
         'emote': '<:Maypul:547182462015504404>',
-        'icon': 'https://imgur.com/womvfUo.png'
+        'icon': 'https://blair-c.github.io/roaa/img/maypul/icon.png'
     },
     'Sylvanos': {
         'color': 9082654,
         'emote': '<:Sylvanos:547182436161945601>',
-        'icon': 'https://imgur.com/DUvl1o8.png'
+        'icon': 'https://blair-c.github.io/roaa/img/sylvanos/icon.png'
     },
     'Olympia': {
         'color': 12091475,
         'emote': '<:Olympia:937869101697761291>',
-        'icon': ''
+        'icon': 'https://blair-c.github.io/roaa/img/olympia/icon.png'
     },
     'Wrastor': {
         'color': 10702538,
         'emote': '<:Wrastor:547182420462665738>',
-        'icon': 'https://imgur.com/YQrYfuv.png'
+        'icon': 'https://blair-c.github.io/roaa/img/wrastor/icon.png'
     },
     'Absa': {
         'color': 15372270,
         'emote': '<:Absa:547182403144384532>',
-        'icon': 'https://imgur.com/y0x3uUH.png'
+        'icon': 'https://blair-c.github.io/roaa/img/absa/icon.png'
     },
     'Elliana': {
         'color': 12151190,
         'emote': '<:Elliana:547182393908396052>',
-        'icon': 'https://i.imgur.com/SIFBYfg.png'
+        'icon': 'https://blair-c.github.io/roaa/img/elliana/icon.png'
     },
     'Pomme': {
         'color': 16504300,
         'emote': '<:Pomme:937869156085268531>',
-        'icon': ''
+        'icon': 'https://blair-c.github.io/roaa/img/pomme/icon.png'
     },
     'Ori': {
         'color': 16315900,
         'emote': '<:Ori:547182375956774920>',
-        'icon': 'https://imgur.com/uTV41fW.png'
+        'icon': 'https://blair-c.github.io/roaa/img/ori/icon.png'
     },
     'Shovel Knight': {
         'color': 15774989,
         'emote': '<:ShovelKnight:547182360219746349>',
-        'icon': 'https://imgur.com/2BvZbjO.png'
+        'icon': 'https://blair-c.github.io/roaa/img/shovelknight/icon.png'
     }
 }
 
