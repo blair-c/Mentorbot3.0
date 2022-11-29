ab0df3e4496ca8e5e113374a89f63eab2965effe
Blair C <bscardiel@gmail.com>
2022-11-28 20:17:06 -0500

    remove on_ready, touch up versioning/logging

--- bot.py
+++ bot.py
@@ -1,20 +1,13 @@
 import os
 import sys
-from itertools import cycle
 from typing import Literal
 
-try:
-    import discord
-    from discord.ext import commands, tasks
-except ImportError:
-    print('Discord.py is not installed', file=sys.stderr)
-    sys.exit(1)
+import discord
+from discord.ext import commands, tasks
+import tabulate
 
-correct_ver = '2.1.0'
-if discord.__version__ != correct_ver:
-    print(f'Discord.py version: {discord.__version__}.\n'
-          f'Please install version {correct_ver}', file=sys.stderr)
-    sys.exit(1)
+print(f'Python {sys.version}\n'
+      f'discord.py {discord.__version__} | tabulate {tabulate.__version__}')
 
 
 class MyBot(commands.Bot):
@@ -27,12 +20,13 @@ class MyBot(commands.Bot):
             'hitboxes',  # Frame data and hitbox commands
             'info',      # Links and informational commands
             'mentors',   # Mentor list and management commands
-            'meta',      # About, help, and misc. commands
+            'about',      # About, help, and misc. commands
             'roles',     # Roles channel setup and functionality
         ]
         for cog in cogs:
             await self.load_extension(f'cogs.{cog}')
             print(f'Loaded {cog} cog...')
+        print(f'Logged in as {bot.user}\nUser ID: {bot.user.id}')
 
 
 intents = discord.Intents.default()
@@ -53,13 +47,6 @@ async def sync(ctx: commands.Context, scope: Literal['global', 'guild']):
             synced = await ctx.bot.tree.sync(guild=ctx.guild)
             txt = 'to the current guild'
     await ctx.send(f'Synced {len(synced)} commands {txt}')
-
-# Start
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Game('discord.me/mentor'))
-    print(f'Logged in as {bot.user}\nUser ID: {bot.user.id}\n'
-          f'{len(bot.guilds):,} guilds / {len(bot.users):,} users')
 
 if __name__ == '__main__':
     bot.run(os.getenv('TOKEN'))  # API Key from environment
