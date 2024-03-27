27d3d8c4850f6965e55593fd7620c405d37cdaf2
blair-c <bscardiel@gmail.com>
2024-03-27 18:25:53 -0400

    update and remove various info commands

--- cogs/info.py
+++ cogs/info.py
@@ -50,31 +50,6 @@ class Info(commands.Cog):
         embed.set_thumbnail(url='https://i.imgur.com/nMS0QPT.png')
         await interaction.response.send_message(content=link, embed=embed)
 
-    @app_commands.command(name='hurtboxdata')
-    async def hurtbox_data_doc(self, interaction: discord.Interaction):
-        """IGL's hurtbox measurements doc"""
-        link = ('https://docs.google.com/spreadsheets/d/'
-                '1jtfuDGjHXfC0UXbEyw7JOZYjB4ufZg6HbF39iNMHxbw')
-        embed = discord.Embed(
-            url=link,
-            title='Rivals Hurtbox Sizes',
-            description='Idle, crouch, and hitstun hurtbox size measurements by IGL.')
-        embed.set_thumbnail(url='https://i.imgur.com/nN6DAmT.png')
-        await interaction.response.send_message(content=link, embed=embed)
-
-    @app_commands.command(name='generalstats')
-    async def general_stats_doc(self, interaction: discord.Interaction):
-        """SNC's general stats doc"""
-        link = ('https://docs.google.com/spreadsheets/d/'
-                '14JIjL_5t81JHqnJmU6BSsRosTe2JO8sFGUysM_9tDoU')
-        embed = discord.Embed(
-            url=link,
-            title='Rivals General Stats - Updated for 1.4.0',
-            description='Data extracted from devmode files and formatted by Kisuno. '
-                        'Info provided by Menace13, Youngblood and SNC.')
-        embed.set_thumbnail(url='https://i.imgur.com/5Iy3ZrX.png')
-        await interaction.response.send_message(content=link, embed=embed)
-
     @app_commands.command(name='igl')
     async def igl_google_docs(self, interaction: discord.Interaction):
         """IGL's list of informational graphics, data, and tools"""
@@ -104,6 +79,7 @@ class Info(commands.Cog):
     @app_commands.command(name='parry')
     async def dodge_data(self, interaction: discord.Interaction):
         """Universal parry, roll, and airdodge frame data"""
+        link = 'https://rivals.academy/library/glossary/#dodges'
         embed = discord.Embed(description=
             # Parry
             ('**Parry** ```ml\n'
@@ -126,72 +102,89 @@ class Info(commands.Cog):
             'Endlag       | 12   \n'
             'FAF          | 27```'))
         embed.set_author(name='Universal Dodge Frame Data')
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(content=link, embed=embed)
 
     @app_commands.command(name='formulas')
     async def formulas_kb_hs_hp(self, interaction: discord.Interaction):
         """Rivals' knockback, hitstun, and hitpause formulas"""
+        link = 'https://rivals.academy/library/glossary/#knockback'
         embed = discord.Embed(description=
             # Knockback
             ('**Knockback** ```ml\n'
-             'BKB + Damage * Knockback_Scaling * 0.12 * Knockback_Adj```'
+             'BKB + (KB_Scaling × Percent_After_Hit × KB_Adj × 0.12)```'
             # Hitstun
             '\n**Hitstun** ```ml\n'
-             'BKB * 4 * ((Knockback_Adj - 1) * 0.6 + 1) + Damage * '
-             '0.12 * Knockback_scaling * 4 * 0.65 * Knockback_Adj```'
+             'Hitstun_Multiplier × ((BKB × (KB_Adj × 2.4 + 1.6)) + '
+             '(KB_Scaling × Percent_After_Hit × KB_Adj × 0.312))```'
             # Hitpause
             '\n**Hitpause** ```ml\n'
-             'Base_Hitpause + Damage * Hitpause_scaling * .05```'))
+             'Base_Hitpause + (Hitpause_Scaling × Percent_After_Hit × 0.05) + Extra_Hitpause```'))
         embed.set_author(name='Rivals Formulas')
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(content=link, embed=embed)
 
     @app_commands.command(name='angleflippers')
     async def angle_flippers(self, interaction: discord.Interaction):
         """List of angle flipper definitions"""
+        link = 'https://rivals.academy/library/glossary/#angle-flippers'
         definitions = ('```glsl\n'
-            '0 - Sends at the exact knockback_angle every time\n'
-            '1 - Sends away from center of the enemy player\n'
-            '2 - Sends toward center of the enemy player\n'
+            '0 - Sends at the exact knockback angle every time\n'
+            '1 - Sends away from the center of the attacker or projectile\n'
+            '2 - Sends toward the center of the attacker or projectile\n'
             '3 - Horizontal knockback sends away from the center of the hitbox\n'
             '4 - Horizontal knockback sends toward the center of the hitbox\n'
             '5 - Horizontal knockback is reversed\n'
-            '6 - Horizontal knockback sends away from the enemy player\n'
-            '7 - Horizontal knockback sends toward the enemy player\n'
+            '6 - Horizontal knockback sends away from the center of the attacker or projectile\n'
+            '7 - Horizontal knockback sends toward the center of the attacker or projectile\n'
             '8 - Sends away from the center of the hitbox\n'
-            '9 - Hits toward the center of the hitbox\n'
-            '10 - Sends in the direction the player is moving```')
+            '9 - Sends toward the center of the hitbox\n'
+            '10 - Sends in the direction the attacker is moving```')
         embed = discord.Embed(title='Angle Flipper Definitions', description=definitions)
-        await interaction.response.send_message(embed=embed)
-
-    @app_commands.command(name='forceflinch')
-    async def force_flinch(self, interaction: discord.Interaction):
-        """List of force flinch definitions"""
-        definitions = ('```glsl\n'
-            '1 - Forces a flinch, unless the attack is crouch cancelled\n'
-            '2 - Cannot cause flinch, even if crouch cancelled\n'
-            '3 - Can always be crouch cancelled, regardless of percent```')
-        embed = discord.Embed(title='Force Flinch Definitions', description=definitions)
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(content=link, embed=embed)
     
     @app_commands.command(name='teching')
-    async def force_flinch(self, interaction: discord.Interaction):
+    async def teching(self, interaction: discord.Interaction):
         """Teching frame data comparison"""
-        await interaction.response.send_message(
-            'https://cdn.discordapp.com/attachments/'
-            '376248878334214145/1160821640221962341/'
-            'roaknockdownframedata.png')
+        link = 'https://rivals.academy/library/glossary/#teching'
+        embed = discord.Embed(description=
+            # Tech In Place
+            ('**Tech In Place** ```ml\n'
+            'Intangible | 1-14       \n'
+            'Endlag     | 4          \n'
+            'FAF        | 19```'
+            # Tech Roll
+            '\n**Tech Roll** ```ml\n'
+            'Intangible | 1-20    \n'
+            'Endlag     | 14      \n'
+            'FAF        | 35```'
+            # Missed (Hitstun)
+            '\n**Missed (Hitstun)** ```ml\n'
+            'Endlag | 13            \n'
+            'FAF    | 14```'
+            # Missed (Tumble)
+            '\n**Missed (Tumble)** ```ml\n'
+            'Endlag | 7             \n'
+            'FAF    | 8```'
+            # Wall Tech
+            '\n**Wall Tech** ```ml\n'
+            'Intangible | 1-15    \n'
+            'FAF        | 12      \n'
+            'Airdodge/Jump Cancellable 4-11```'
+            # Ceiling Tech
+            '\n**Ceiling Tech** ```ml\n'
+            'Intangible | 1-15       \n'
+            'FAF        | 12         \n'
+            'IASA Cancellable 1-19```'))
+        embed.set_author(name='Universal Teching Frame Data')
+        embed.set_image(url='https://cdn.discordapp.com/attachments/'
+                            '376248878334214145/1160821640221962341/'
+                            'roaknockdownframedata.png')
+        await interaction.response.send_message(content=link, embed=embed)
 
     # Misc.
     @app_commands.command(name='fpsfix')
     async def fps_fix(self, interaction: discord.Interaction):
         """60 fps fix instructions for Nvidia graphics cards"""
         link = 'https://twitter.com/darainbowcuddle/status/1410724611327631364'
-        await interaction.response.send_message(link)
-
-    @app_commands.command(name='parrybot')
-    async def axmos_parry_bot(self, interaction: discord.Interaction):
-        """Axmos' parry practice bot"""
-        link = 'https://axmos.itch.io/axmos-parry-bot'
         await interaction.response.send_message(link)
 
     @app_commands.command(name='replays')
